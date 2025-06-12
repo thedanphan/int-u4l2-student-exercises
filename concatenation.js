3db97c349e7fd7324e879042dabb7d5eca7b659c
ColdAsianrice <dphan4206@gmail.com>
2025-06-12 14:50:13 -0500

    Applied string concatenation in Javascript!

--- concatenation.js
+++ concatenation.js
@@ -7,13 +7,13 @@ submitButton.addEventListener("click", function() {
     let mood = document.querySelector(".mood").value;
     let food = document.querySelector(".food-name").value;
     let birthYear = document.querySelector(".year").value;
-    let age = 2023 - birthYear; // update to 2024, if necessary
+    let age = 2025 - birthYear; // update to 2024, if necessary
     // 1. Declare a variable to save the value of the city the user is from.
-
+    let  cityValue = document.querySelector(".city").value;
     // 2. Below write your sentences! 
     // - Have them show up in the div with class "results".
     // - Example paragraph: "My name is (myName). I'm feeling (mood) and I absolutely lovveeee eating (food) 😍..."
     // - Try placing your sentences into <p> or <li> tags.
-
-
+    let sentence = `<p>My name is ${myName}. I'm feeling ${mood} and I absolute love eating ${food}. I was born in ${birthYear} and I live in ${cityValue}.</p>`
+    results.innerHTML = sentence;
 });
